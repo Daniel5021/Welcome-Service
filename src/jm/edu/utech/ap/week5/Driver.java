ac396e85b0325e046407dd9c0ee1d65762d1960a
chinloyal <jordain7@gmail.com>
2018-09-27 14:45:06 -0500

    Added Diver and AppEngine

---
+++ src/jm/edu/utech/ap/week5/Driver.java
@@ -0,0 +1,8 @@
+package jm.edu.utech.ap.week5;
+
+public class Driver implements Driveable{
+
+	public void drive(float numberOfMiles) {
+		System.out.println("Chinloy has drived " + numberOfMiles + " miles.");
+	}
+}
